4692312cc06d3bc6a6cbccaedb3428c4f8a15ad4
Alex Yom <alexyom24@gmail.com>
2021-02-23 20:58:33 -0500

    firstdjango

---
+++ django_intro/firstDjango/appOne/urls.py
@@ -0,0 +1,27 @@
+"""firstDjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+]
+urlpatterns = [
+        path('bears', views.one_method),                        # would only match localhost:8000/bears
+        path('bears/<int:my_val>', views.another_method),       # would match localhost:8000/bears/23
+        path('bears/<str:name>/poke', views.yet_another),       # would match localhost:8000/bears/pooh/poke
+    	path('<int:id>/<str:color>', views.one_more),           # would match localhost:8000/17/brown
+]
