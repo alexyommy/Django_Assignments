e7e8fa9484776d32788e4f146eda5f67271637d7
Alex Yom <alexyom24@gmail.com>
2021-03-05 22:49:07 -0500

    users w template

--- django_orm/single_model_orm/users_app/urls.py
+++ django_orm/single_model_orm/users_app/urls.py
@@ -18,4 +18,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('add_user', views.add_user),
 ]
--- django_orm/single_model_orm/users_app/views.py
+++ django_orm/single_model_orm/users_app/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import User
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "all_the_users": User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add_user(request):
+    User.objects.create(
+        first_name= request.POST['first_name'],
+        last_name= request.POST['last_name'],
+        email_address= request.POST['email_address'],
+        age= request.POST['age'],
+    )
+    return redirect("/")
