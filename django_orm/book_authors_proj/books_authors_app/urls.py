b6813bf5d120c860cc4ae4dff2d8d071eae606ac
Alex Yom <alexyom24@gmail.com>
2021-03-09 21:08:56 -0500

    books_authors_app

--- django_orm/book_authors_proj/books_authors_app/urls.py
+++ django_orm/book_authors_proj/books_authors_app/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.index),
+    path('add_book', views.add_book)
 ]
--- django_orm/book_authors_proj/books_authors_app/views.py
+++ django_orm/book_authors_proj/books_authors_app/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import book, author
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {
+        'books': book.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add_book(request):
+    book.objects.create(
+        title=request.POST['title'],
+        # desc=request.POST['desc']
+    )
+    return redirect('/')
