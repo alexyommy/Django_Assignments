e8364f1c5d3afaf8315c7e6e4db897a56ad3f228
Alex Yom <alexyom24@gmail.com>
2021-03-13 19:02:05 -0500

    validations

--- django_orm/semi_restful_tv_shows/tv_shows_app/models.py
+++ django_orm/semi_restful_tv_shows/tv_shows_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class showManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Show title should be at least 2 charcters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Show network should be at least 3 charcters"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "Show description should be at least 10 characters"
+        return errors
+
 class show(models.Model):
     title = models.CharField(max_length=45)
     network = models.CharField(max_length=45)
@@ -8,3 +19,8 @@ class show(models.Model):
     desc = models.TextField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = showManager()
+
+
+
+
--- django_orm/semi_restful_tv_shows/tv_shows_app/views.py
+++ django_orm/semi_restful_tv_shows/tv_shows_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import redirect, render
+from django.contrib import messages
+
 from .models import *
+
 
 # Create your views here.
 def index(request):
@@ -12,13 +15,19 @@ def new(request):
     return render(request, 'new.html')
 
 def create(request):
-    show.objects.create(
-        title = request.POST['title'],
-        network = request.POST['network'],
-        release_date = request.POST['release_date'],
-        desc = request.POST['desc']
-    )
-    return redirect('/shows/')
+    errors = show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        show.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            release_date = request.POST['release_date'],
+            desc = request.POST['desc']
+        )
+        return redirect('/shows/')
 
 def show_page(request, show_id):
     Show = show.objects.get(id=show_id)
@@ -40,10 +49,20 @@ def edit(request, show_id):
     return render(request, "edit.html", context)
 
 def update(request, show_id):
-    Show = show.objects.get(id = show_id)
-    Show.title = request.POST['title']
-    Show.network = request.POST['network']
-    Show.release_date = request.POST['release_date']
-    Show.desc = request.POST['desc']
-    Show.save()
-    return redirect('/shows')
+    errors = show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/edit'+id)
+    else:
+        Show = show.objects.get(id = show_id)
+        Show.title = request.POST['title']
+        Show.network = request.POST['network']
+        Show.release_date = request.POST['release_date']
+        Show.desc = request.POST['desc']
+        Show.save()
+        return redirect('/shows')
+
+# Inside your app's views.py file
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
